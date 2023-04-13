d90c787d0ee2d1d36af903b73952573f3d90d6cb
Izzataa <129064478+Izzataa@users.noreply.github.com>
2023-04-13 13:36:10 +0400

    Add files via upload

---
+++ BookApp.App/BookApp.App/Program.cs
@@ -0,0 +1,58 @@
+﻿using BookApp.App.Services;
+using BookApp.Core.Models;
+
+    BookService bookService = new BookService();
+do
+{
+    
+    Console.WriteLine("=====Welcome Book Create======\n");
+    Console.WriteLine("0.Close app");
+    Console.WriteLine("1.Book list:");
+    Console.WriteLine("2.Book add:");
+    string choose = Console.ReadLine();
+    int chooseNum;
+    while (!int.TryParse(choose, out chooseNum) || chooseNum < 0 || chooseNum > 2)
+    {
+        Console.WriteLine("choose correctly:");
+        int.TryParse(choose=Console.ReadLine(), out chooseNum);
+    }
+
+    switch (chooseNum)
+    {
+        case 0:
+            return;
+        case 1:
+            ShowBook(ref bookService);
+            break;
+        case 2:
+
+            Addbook(ref bookService);
+            break;
+    }
+} while (true);
+
+
+static void Addbook(ref BookService bookService)
+{
+    Console.WriteLine("input name");
+    string ChoseBookname = Console.ReadLine();
+    while ((ChoseBookname) ==null)
+    {
+        Console.WriteLine("choose correctly:");
+        ChoseBookname = Console.ReadLine();
+    }
+    Console.WriteLine("input price");
+    string ChoseBookprice=Console.ReadLine();
+    double price;
+    double.TryParse(ChoseBookprice, out price);
+    while ((ChoseBookprice) == null)
+    {
+        Console.WriteLine("choose correctly:");
+        double.TryParse(ChoseBookprice=Console.ReadLine(), out price);
+    }
+    bookService.CreateBook(ChoseBookname, price);
+}
+static void ShowBook(ref BookService bookService)
+{
+    bookService.ShowBooks();
+}
---
+++ BookApp.App/BookApp.App/Services/BookService.cs
@@ -0,0 +1,38 @@
+﻿using BookApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookApp.App.Services
+{
+    public class BookService
+    {
+        private Book[] _books;
+
+        public Book[] Books { get => _books; }
+        public BookService()
+        {
+            _books= new Book[0];
+        }
+       
+        public void CreateBook(string name,double price)
+        {
+         Book book =new Book(name,price);
+         Array.Resize(ref _books,    _books.Length + 1);
+         _books[_books.Length - 1] = book;
+        }
+        public void ShowBooks()
+        {
+            if (_books.Length>0)
+            {
+                foreach (Book book in _books)
+                {
+                    Console.WriteLine($"Name:{book.Name} Price:{book.Price}");
+                }
+            }
+            Console.WriteLine("Book empty");
+        }
+    }
+}
